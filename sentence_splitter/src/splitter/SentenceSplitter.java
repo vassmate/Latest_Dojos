9ab12e1e4fa691db972301ac72688cf8a1ef9058
vassmate <vassmate08@gmail.com>
2016-10-21 11:16:49 +0200

    Adding sentence splitter task

---
+++ sentence_splitter/src/splitter/SentenceSplitter.java
@@ -0,0 +1,52 @@
+package splitter;
+
+public class SentenceSplitter {
+
+	private String sentence;
+
+	public SentenceSplitter(String input) {
+		sentence = input;
+	}
+
+	public String getSentence() {
+		return sentence;
+	}
+
+	public void splitSentence(int rowLength) {
+		String[] splittedSentence = sentence.split(" ");
+		StringBuilder result = new StringBuilder();
+		int currentRowLength = 0;
+		for (String word : splittedSentence) {
+			if (word.length() > rowLength) {
+				String sw = splitWord(word, rowLength);
+				String we = word.substring(sw.length() - 1, word.length());
+				result.append("\n" + sw + "\n" + we + " ");
+				currentRowLength = we.length();
+			} else if (currentRowLength + word.length() >= rowLength) {
+				result.append("\n" + word + " ");
+				currentRowLength = word.length() + 1;
+			} else {
+				result.append(word + " ");
+				currentRowLength += word.length() + 1;
+			}
+		}
+		sentence = result.toString();
+	}
+
+	private String splitWord(String word, int rowLength) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < rowLength; i++) {
+			char currentChar = word.charAt(i);
+			sb.append(currentChar);
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		String inputSentence = "Java is a high-level programming language originally developed by Sun Microsystems and released in 1995.";
+		System.out.println(inputSentence + "\n");
+		SentenceSplitter sSplitter = new SentenceSplitter(inputSentence);
+		sSplitter.splitSentence(6);
+		System.out.println(sSplitter.getSentence());
+	}
+}
